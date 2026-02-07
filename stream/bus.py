6ca79e5cc4fa64ecc8995d2dd907e29aa1bf847e
Kusumar95 <51442500+Kusumar95@users.noreply.github.com>
2026-02-07 14:10:42 +0400

    Add files via upload

--- stream/bus.py
+++ stream/bus.py
@@ -1,1 +1,5 @@
 
+import asyncio
+TOPICS={k:asyncio.Queue() for k in ["dau","trade","completeness"]}
+async def publish(t,m): await TOPICS[t].put(m)
+async def consume(t): return await TOPICS[t].get()
